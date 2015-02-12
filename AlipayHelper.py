45346fcbdd4f826b9dd22b84cf26ff1261a2e8a1
seanchense <seanchense@outlook.com>
2015-02-12 21:38:16 +0800

    modify print

--- AlipayHelper.py
+++ AlipayHelper.py
@@ -11,12 +11,10 @@ device = MonkeyRunner.waitForConnection()
 w = 1080
 h = 1780
 
-# raw_input("按 Enter后再按 Ctrl+D 后开始乱点")
 print('go')
 while True:
     time.sleep(0.4)
     wi = random.randint(0, w)
     hi = random.randint(40,h)
     print (wi + hi)
-    # print("now ~~> x: " + str(wi) + " y: " + str(hi)
     device.touch(wi, hi, MonkeyDevice.DOWN_AND_UP)
